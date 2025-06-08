1b26d289e31714fee77d9fb171f61cf48e0bd9db
Andres Rueda <camilo20021409@gmail.com>
2025-06-08 13:31:32 -0500

    perfil

--- lib/presentation/Screens/perfil/perfil_screen.dart
+++ lib/presentation/Screens/perfil/perfil_screen.dart
@@ -34,28 +34,74 @@ class PerfilScreen extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: const <Widget>[
-                TextoA(text: "edad:", width: 0.35),
+                TextoA(text: "Edad:", width: 0.35),
                 TextoA(text: "22 años", width: 0.5, aling: 1),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: const <Widget>[
-                TextoA(text: "fecha de nacimiento:", width: 0.35),
+                TextoA(text: "Fecha de nacimiento:", width: 0.35),
                 TextoA(text: "14/09/2002", width: 0.5, aling: 1),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: const <Widget>[
-                TextoA(text: "tipo de sangre:", width: 0.35),
+                TextoA(text: "Tipo de sangre:", width: 0.35),
                 TextoA(text: "O+", width: 0.5, aling: 1),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: const <Widget>[
-                TextoA(text: "Medicamentos formulados", width: 0.9),
+                TextoA(text: "Dirección:", width: 0.35),
+                TextoA(text: "14/09/2002", width: 0.5, aling: 1),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                TextoA(text: "Correo:", width: 0.35),
+                TextoA(text: "O+", width: 0.5, aling: 1),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                TextoA(text: "teléfono", width: 0.35),
+                TextoA(text: "14/09/2002", width: 0.5, aling: 1),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                TextoA(
+                  text: "Medicamentos formulados",
+                  width: 0.35,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                TextoA(
+                  text: "Tiempo consumo",
+                  width: 0.35,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              ],
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                TextoA(text: "ibuprofeno:", width: 0.35),
+                TextoA(text: "2 Semanas", width: 0.5, aling: 1),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const <Widget>[
+                TextoA(text: "Paracetamol:", width: 0.35),
+                TextoA(text: "1 mes", width: 0.5, aling: 1),
               ],
             ),
           ],
@@ -69,11 +115,13 @@ class TextoA extends StatelessWidget {
   final String text;
   final double width;
   final int aling;
+  final TextStyle? style;
   const TextoA({
     super.key,
     required this.text,
     required this.width,
     this.aling = 0,
+    this.style,
   });
   @override
   Widget build(BuildContext context) {
@@ -86,7 +134,7 @@ class TextoA extends StatelessWidget {
         // Ancho específico para este Text
         child: Text(
           text,
-          style: TextStyle(fontWeight: FontWeight.bold),
+          style: style,
           textAlign: aling == 0 ? TextAlign.left : TextAlign.right,
         ),
       ),
